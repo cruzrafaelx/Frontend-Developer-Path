78aca0ed29938de87b017b06af61dce53549831c
cruzrafaelx <rafaelcruz.code@gmail.com>
2024-04-10 05:00:57 +0200

    add working add order to cart function

--- M5. Essential Javascript Concepts/5. Restaurant Ordering App/index.js
+++ M5. Essential Javascript Concepts/5. Restaurant Ordering App/index.js
@@ -2,6 +2,7 @@ import { menuArray } from "./data.js";
 
 const orderView = document.getElementById("order-view")
 
+let orderList = []
 
 document.addEventListener("click", function(e) {
 
@@ -17,16 +18,47 @@ function handleAddButton(n){
           return item.id == n
     })[0]
 
-}
+    orderList.push(food)
+    console.log(orderList)
 
+    if(orderList.length >= 1){
+        orderView.classList.remove("invisible")
+    }
+
+    renderOrders()
+}
 
 
 
 
 let htmlFeed = ``
 
- function render(){
 
+function getOrders(){
+    
+    let htmlOrders = ``
+
+    orderList.forEach(food =>{
+        htmlOrders += `
+        <section class="order-line flex">
+                <section class="flex order-line-left">
+                    <h3 class="food-name">${food.name}</h3>
+                    <button class="rmv-btn">remove</button>
+                </section>
+            <section class="order-line-right">
+                <p>${food.price}</p> 
+            </section>   
+        </section>
+        `
+    })
+
+   return htmlOrders
+}
+
+
+
+ function getFeedHtml(){
+ 
       menuArray.forEach(item => {
 
             htmlFeed += `
@@ -48,9 +80,19 @@ let htmlFeed = ``
             </section>
             `
       })
-
       return htmlFeed
  }
 
-document.getElementById("feed").innerHTML = render()
+
+
+function renderOrders(){
+    document.getElementById("order-section").innerHTML = getOrders()
+}
+
+ function render(){
+    document.getElementById("feed").innerHTML = getFeedHtml()
+ }
+
+ render()
+
 
