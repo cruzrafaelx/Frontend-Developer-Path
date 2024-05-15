80de8df3840d7f9b176e5b25cb7d362e5c6c15fc
cruzrafaelx <rafaelcruz@outlook.de>
2024-05-15 02:03:14 +0200

    refactor getMovies function

--- M8. APIs/Movie Watchlist/index.js
+++ M8. APIs/Movie Watchlist/index.js
@@ -11,65 +11,59 @@ const popMoviesURL =  'https://api.themoviedb.org/3/movie/popular?language=en-US
 const posterURL = 'https://image.tmdb.org/t/p/w500'
 
 
-function getMovies(url, options){
-      fetch(url, options).then(res => res.json()).then(data => {
+async function getMovies(url, options){
+      const res = await fetch(url, options)
+      const data = await res.json()
             console.log(data.results)
 
             let html = ''
             
-      
             for(let i=0; i < data.results.length; i++){
 
-
-                  const {title, poster_path, vote_average, overview, genre_ids} = data.results[i]
-
-                  //Array that contains the genres
-                  let genreList = []
-
-                  //Handle the promise of async function getGenres results
-                   getGenres(genre_ids[0], genre_ids[1], genre_ids[2])
-                   .then((result) => {
-                        genreList.push(result[0], result[1], result[2])
-                   })                   
-
-                   
-            
-                  html += `
-                  <div class="mov-box">
-                  <img src="${posterURL + poster_path}">
-
-                  <div class="mov-desc">
-                        <div class="desc-top">
-                              <h4>${title}</h4>
-                              <p>${vote_average.toFixed(2)}</p>
-                        </div>
-                        <div class="desc-mid">
-                              <p>${genreList[0], genreList[1], genreList[2]}</p>
-                              <button>+Watchlist</button>
-                              <p></p>
-                        </div>
-                        <div class="desc-bot">
-                              <p>${overview}</p>
-                        </div>
-                  </div>
-            </div>
+            //Deconstruction of the resolved promise
+            const {title, poster_path, vote_average, overview, genre_ids} = data.results[i]
                   
-                  `
+                  //Call getGenres function
+                  getGenres(genre_ids[0], genre_ids[1], genre_ids[2])
+                   .then((res) => {
+                                                
+                        html += `
+                        <div class="mov-box">
+                              <img src="${posterURL + poster_path}">
+                        
+                              <div class="mov-desc">
+                                    <div class="desc-top">
+                                          <h4>${title}</h4>
+                                          <p>${vote_average.toFixed(2)}</p>
+                                    </div>
+                                    <div class="desc-mid">
+                                          <p>${res.join(', ')}</p>
+                                          <button>+Watchlist</button>
+                                          <p></p>
+                                    </div>
+                                    <div class="desc-bot">
+                                          <p>${overview}</p>
+                                    </div>
+                              </div>
+                        </div>
+                        
+                        `
+                        movContainer.innerHTML = html
+                   })                        
             }
-          
-            movContainer.innerHTML = html
-      })
-}
+      }
+
     
 getMovies(popMoviesURL, options)
 
-
+//Function to get the genres using the genres_id
 async function getGenres(param, param1, param2){
       
       let genresArray = []
 
       const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?language=en', options)
       const data = await res.json()
+
       const {genres} = data
 
       for(let i=0; i < genres.length; i++){
@@ -88,8 +82,5 @@ async function getGenres(param, param1, param2){
       }
 
      return genresArray
-     console.log(genresArray)
-
 }
 
-console.log(getGenres(878, 27, 28))
