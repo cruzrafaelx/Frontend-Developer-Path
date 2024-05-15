804df2ee88e9610f2a386e1c35b2b826f9a71424
cruzrafaelx <rafaelcruz@outlook.de>
2024-05-15 23:03:50 +0200

    Add default background image if err

--- M8. APIs/Promise Rejection/index.js
+++ M8. APIs/Promise Rejection/index.js
@@ -1,10 +1,27 @@
 
+const author = document.getElementById("author")
+
 
 //Get random photo from unsplash api
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=beach')
 .then(res => res.json())
 .then(data => {
       
+      console.log(data)
+
+      const defaultImg = data.urls.full
       const imageUrl = data.links.download
-      document.body.style.backgroundImage = "url('" + imageUrl +"')"
+      document.body.style.backgroundImage = `url('${imageUrl})`
+      
+      author.innerHTML = `
+      By: ${data.user.first_name +' '+ data.user.last_name}
+      `
+    //The catch method will run if any errors are thrown to the .then() handlers
+}).catch(err => {
+      document.body.style.backgroundImage = `url("https://images.unsplash.com/photo-1594235206348-f03d8c4ebda0?crop=entropy&cs=srgb&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTU4MDY5MDh8&ixlib=rb-4.0.3&q=85")`
+
+      author.innerHTML = `
+      By: Süleyman Coskun
+      `
 })
+      
