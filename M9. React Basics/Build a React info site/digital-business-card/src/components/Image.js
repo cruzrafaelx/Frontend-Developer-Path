5b28d4162a56d8456e038bc5b39050085685e53a
cruzrafaelx <rafaelcruz@outlook.de>
2024-05-31 02:33:33 +0200

    Add components to index.js

---
+++ M9. React Basics/Build a React info site/digital-business-card/src/components/Image.js
@@ -0,0 +1,9 @@
+import Avatar from "../images/avatar.jpg"
+
+export default function Image(){
+      return(
+            <div>
+                  <img src={Avatar} alt="user avatar" height="100px"></img>
+            </div>
+      )
+}
--- M9. React Basics/Build a React info site/digital-business-card/src/index.js
+++ M9. React Basics/Build a React info site/digital-business-card/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Header from './components/Header';
+import Image from './components/Image';
+import About from './components/About';
+import Footer from './components/Footer'
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Image />
+    <Header />
+    <About />
+    <Footer />
+   
   </React.StrictMode>
 );
 
