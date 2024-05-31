ee0bb7a81d695b9cfb3757edc47e3110789f7940
cruzrafaelx <rafaelcruz@outlook.de>
2024-05-31 18:52:01 +0200

    Add main content

---
+++ M9. React Basics/Build a React info site/digital-business-card/src/components/Main.js
@@ -0,0 +1,17 @@
+export default function Main(){
+      return(
+            <div className="main">
+
+                  <div className="about">
+                        <h2>About</h2>
+                        <p>I am deeply passionate about web development since November 2023. By night, I thrive in the hotel industry, but my days are devoted to mastering code.</p>
+                  </div>
+                  
+                  <div className="interests">
+                        <h2>Interests</h2>
+                        <p>Cooking, singing, and language learning ignite my soul outside of coding. I also like coffee a lot.</p>
+                  </div>
+                  
+            </div>
+      )
+}
--- M9. React Basics/Build a React info site/digital-business-card/src/components/Parent.js
+++ M9. React Basics/Build a React info site/digital-business-card/src/components/Parent.js
@@ -1,15 +1,15 @@
 import Image from './Image';
-import About from './About';
+import Main from './Main';
 import Footer from './Footer'
 import Header from './Header'
-import '../Parent.css'
+
 
 export default function Parent(){
       return(
             <div className='parent'>
                   <Image />
                   <Header />
-                  <About />
+                  <Main />
                   <Footer /> 
             </div>
       )
