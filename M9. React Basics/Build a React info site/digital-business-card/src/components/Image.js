bffb1bd2f21f7e593fec670263254833c8487fc0
cruzrafaelx <rafaelcruz@outlook.de>
2024-05-31 19:34:15 +0200

    Add css to footer component and background color to body

--- M9. React Basics/Build a React info site/digital-business-card/src/components/Image.js
+++ M9. React Basics/Build a React info site/digital-business-card/src/components/Image.js
@@ -2,7 +2,7 @@ import Avatar from "../images/avatar.jpg"
 
 export default function Image(){
       return(
-            <div>
+            <div className="avatar">
                   <img src={Avatar} className="avatar" alt="user avatar" height="100px"></img>
             </div>
       )
