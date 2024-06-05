73a57700dbc0491e0ebb201f1c8c4e34b14fe770
cruzrafaelx <rafaelcruz@outlook.de>
2024-06-05 23:31:28 +0200

    Add event listener for meme button

--- M9-react-basics/meme-generator/src/Components/Meme.js
+++ M9-react-basics/meme-generator/src/Components/Meme.js
@@ -1,19 +1,50 @@
+import memesData from "../memesData"
+
+
 export default function Meme(){
+
+      function handleMemeButton(e){
+                  
+            e.preventDefault()
+
+            const arrayLength = memesData.data.memes.length
+            const randomNum = Math.floor(arrayLength * Math.random())
+      
+            console.log(memesData.data.memes[randomNum].url)
+      }
+
+
       return(
             <div>
                   <form className="form">
                         <div className="input-container">
-                              <div className="top-container">
-                                    <label className="top-label">Top Text</label>
-                                    <input className="top-input" type="text"></input>
+                              <div className="container">
+                                    <label 
+                                          htmlFor="left-input" className="label"> 
+                                          Top Text
+                                    </label>
+                                    <input 
+                                          id="left-input"  
+                                          className="input" 
+                                          type="text"
+                                          placeholder="shut up">
+                                    </input>
                               </div>
                               
-                              <div className="btm-container">
-                                    <label className="btm-label">Bottom Text</label>
-                                    <input className="btm-input" type="text"></input>
+                              <div className="container">
+                                    <label 
+                                          htmlFor="right-input" className="label">
+                                          Bottom Text
+                                    </label>
+                                    <input 
+                                          id="right-input" 
+                                          className="input" 
+                                          type="text"
+                                          placeholder="and take my money">
+                                    </input>
                               </div>
                         </div>
-                        <button className="btn">Get a new meme</button>
+                        <button onClick={handleMemeButton} className="btn">Get a new meme</button>
                   </form>
                   
             </div>
